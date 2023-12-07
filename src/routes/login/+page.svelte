6f12fb68b39f30a00adda3598fe8f58bc7da491d
Riasat <67420883+riasatsk@users.noreply.github.com>
2023-12-07 21:57:15 +0530

    Update +page.svelte

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,7 +5,7 @@
 	let password;
 	async function handleLogin() {
 		show = true;
-		const response = await fetch('http://localhost:3000/login', {
+		const response = await fetch('api/login', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
