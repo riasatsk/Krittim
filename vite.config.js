6925776c9a5db7f5ac32393fdbab2a230a0672f5
Riasat <67420883+riasatsk@users.noreply.github.com>
2023-12-07 21:51:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			'/api': {
-				target: 'http://localhost:3000',
+				target: 'https://krittim-backend.onrender.com/',
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, '')
 			}
