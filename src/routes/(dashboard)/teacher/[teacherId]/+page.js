7ea2bd9cb0d250749b21f54b43267475bb38d6f3
Riasat <67420883+riasatsk@users.noreply.github.com>
2023-12-09 21:08:49 +0530

    Update +page.js

--- src/routes/(dashboard)/teacher/[teacherId]/+page.js
+++ src/routes/(dashboard)/teacher/[teacherId]/+page.js
@@ -2,13 +2,13 @@ export const load = async (loadEvent) => {
 	const { fetch, params } = loadEvent;
     const {teacherId} = params;
 	const token = window.localStorage.getItem('jwt-Token');
-	const response = await fetch('/api/subject', {
+	const response = await fetch('https://krittim-backend.onrender.com/subject', {
 		method: 'GET',
 		headers: {
 			Authorization: `Bearer ` + token
 		}
 	});
-	const responseOnGetTeachers = await fetch(`/api/teacher/${teacherId}`, {
+	const responseOnGetTeachers = await fetch(`https://krittim-backend.onrender.com/teacher/${teacherId}`, {
 		method: 'GET',
 		headers: {
 			Authorization: `Bearer ` + token
